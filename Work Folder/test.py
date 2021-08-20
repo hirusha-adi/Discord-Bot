1ce3fd2f921bd22f8e858464db02fa7236dcf3ba
Hirusha Adikari <zesta5j7k@gmail.com>
2021-08-20 21:47:11 +0530

    Update test.py

--- Work Folder/test.py
+++ Work Folder/test.py
@@ -1,9 +1,14 @@
 
-no = "123x456"
+# no = "123x456"
 
-# print(no.replace("x", " "))
+# # print(no.replace("x", " "))
 
-x = no.split("x")[0]
+# x = no.split("x")[0]
+
+# print(x)
+
+import random
+
+x = f"{random.randint(1940, 2007)}-{random.randint(1, 12)}-{random.randint(1, 30)}"
 
 print(x)
-
